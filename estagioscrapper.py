d33355d533a55287a68d135f5b36d8c9af862614
Guilherme Lima <guilimadev@gmail.com>
2022-11-15 00:21:49 -0300

    Fixing Selenium

--- estagioscrapper.py
+++ estagioscrapper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 import pandas as pd 
